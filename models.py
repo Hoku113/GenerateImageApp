914bcbd39ad3bcd7eb95beef3c219ed22cfb4f71
Hokuto Uchida <hokuto.u0113@gmail.com>
2024-10-23 08:42:03 +0900

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
 models = {
     "Stablediffusion2" : "stabilityai/stable-diffusion-2-base",
     "アニメ": "Ojimi/anime-kawai-diffusion",
-    "油絵": "stablediffusionapi/painters-checkpoint-oil-p",
-    "写真": "dreamlike-art/dreamlike-photoreal-2.0",
+    "油絵": "stablediffusionapi/painters-checkpoint-oil-p"
   }
